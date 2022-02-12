7d80ce16e2e4d07eb1345fe7ad063d2791168a39
Hehly <1830904228@qq.com>
2022-02-12 16:48:59 +0800

    高级语法识别

---
+++ src/02-primitive-types.ts
@@ -0,0 +1,21 @@
+/**
+ * 原始数据初始类型
+ */
+
+const a:string = "foobar";
+
+const b:number = 10;
+
+const c:boolean = false;
+
+// const d:number = null; // 严格模式 检查null strictNullChecks 
+
+const e:null = null;
+
+const f:undefined = undefined;
+
+const g:symbol = Symbol(); // es6 新增 高级语法 
+/**
+ * "target": "es2016", 第一种es5 但是DOM 找不到
+ * "lib": ["ES2015","DOM"] 第二中
+ */
